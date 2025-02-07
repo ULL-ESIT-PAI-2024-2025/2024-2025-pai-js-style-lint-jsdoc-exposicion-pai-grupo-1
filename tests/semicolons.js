4747c7f48f5da85445adc68cc7460a336b4a1fe9
Carolina Acosta Acosta <115373365+Carolina-AcostaAcosta@users.noreply.github.com>
2025-02-07 07:47:48 +0000

    Create semicolons.js

---
+++ tests/semicolons.js
@@ -0,0 +1,13 @@
+"use strict";
+
+/**
+ * Se ejecuta correctamente
+ */
+console.log("Hello");
+[1, 2].forEach(console.log);
+
+/**
+ * No da el resultado esperado
+ */
+console.log("Hello")
+[1, 2].forEach(console.log);
