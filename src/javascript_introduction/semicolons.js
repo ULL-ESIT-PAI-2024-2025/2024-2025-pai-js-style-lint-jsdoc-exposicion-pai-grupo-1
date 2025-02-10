0af44141d54f944a4d87f2b3213468962bab23ea
Carolina Acosta Acosta <alu0101204574@ull.edu.es>
2025-02-10 08:40:22 +0000

    adding files

--- src/javascript_introduction/semicolons.js
+++ src/javascript_introduction/semicolons.js
@@ -1,9 +1,15 @@
 'use strict';
 
-/** Se ejecuta correctamente */
+console.log('Hello');
+console.log('World');
+
+console.log('Hello')
+console.log('World')
+
+/** It executes correctly */
 console.log('Hello');
 [1, 2].forEach(console.log);
 
-/** No da el resultado esperado */
+/** Doesn't give what we're expecting */
 console.log('Hello')
 [1, 2].forEach(console.log);
