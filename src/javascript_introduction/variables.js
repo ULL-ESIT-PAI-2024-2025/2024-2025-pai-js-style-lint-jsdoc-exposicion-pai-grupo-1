ac9af657c586ba152df231c21f802c2249a3859f
Carolina Acosta Acosta <alu0101204574@ull.edu.es>
2025-02-10 08:54:00 +0000

    Comments

--- src/javascript_introduction/variables.js
+++ src/javascript_introduction/variables.js
@@ -1,3 +1,16 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Programación de Aplicaciones Interactivas
+ *
+ * @author Carolina Acosta Acosta
+ * @author Paulo Padilla Domingues
+ * @author Laura Ramallo Pérez
+ * @since Feb 09 2025
+ * @desc Incorrect Guidestyle
+ */
+
 'use strict';
 
 let hello = 'Hello world!';
